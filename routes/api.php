6e63a62535bec5cb4187ce37417ef40cc2305348
Platinum <platinumemirate@gmail.com>
2022-05-23 17:02:40 +0100

    api get schedule

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SchedulesController;
 use App\Http\Controllers\SubmissionsController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('submission', [SubmissionsController::class, 'submit']);
+Route::get('getsubmissions', [SchedulesController::class, 'getsubmissions']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
